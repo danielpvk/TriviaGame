c6a5f935ba4739b097a8ca7fd1f1e30a3285588d
danielpv <danielpvk@gmail.com>
2018-09-13 02:06:20 -0500

    good

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -11,20 +11,20 @@ var datos ={
     "Who participated in the battle of Isengard?",
     "What were Anakin Skywalker's last words in 'Return of the Jedi'?",
     "Walt’s pre-Heisenberg vehicle, the one he runs over the gangsters in, is a"],
-     answers:["Hannah Montana","iCarly", "1Victorious", "Disney Club",
-      "1Harbor School","Beverly High","Bayview School","Orange High",
-      "Britney Spears","Beyonce","Pink","1Avril Lavigne",
-      "Dawson","Charlie","1Pacey","Jack",
-        "Trumpet","Tuba","1French Horn","Clarinet",
+     answers:["Hannah Montana","iCarly", "Victorious", "Disney Club",
+      "Harbor School","Beverly High","Bayview School","Orange High",
+      "Britney Spears","Beyonce","Pink","Avril Lavigne",
+      "Dawson","Charlie","Pacey","Jack",
+        "Trumpet","Tuba","French Horn","Clarinet",
         "Gigantic National Bank","Global Nutriotional Bank","Grand National Bands","Goliath National Bank",
         "Redoul","Nightshade","Nightblock","Oleander",
-        "1A basilik fang","The sword of gryffindor","Fiendfyre","The killing curse",
-        "Saurumon's forces versus King Theoden Rohrrimn", "1Saurumon's forces versus the Ents","Orcs versus Galadhrim of Lothlorien", "Sauron forces versus Gondor forces",
-        "1You were right about me, tell your sister you were right","I'm sorry","May the force be with you","You are the last jedi, the force runs strong in you, pass on.. what you have learned",
+        "A basilik fang","The sword of gryffindor","Fiendfyre","The killing curse",
+        "Saurumon's forces versus King Theoden Rohrrimn", "Saurumon's forces versus the Ents","Orcs versus Galadhrim of Lothlorien", "Sauron forces versus Gondor forces",
+        "You were right about me, tell your sister you were right","I'm sorry","May the force be with you","You are the last jedi, the force runs strong in you, pass on.. what you have learned",
         "Chevy Citation","Ford Explorer","Nissan Cube","1Pontiac Aztek"],
-     results:[1,0,3,2,2,3,2,0,1,0,3],
+     results:[2,0,3,2,2,3,2,0,1,0,3],
      fillquestion:function(i){
-      debugger;
+     
        i=9-i;
        $(".question").html("<h2>"+this.questions[i]+"</h2>");
        $("#answer1").html("<h3>"+this.answers[i*4+0]+"</h3>");
@@ -81,7 +81,7 @@ var datos ={
    
       trivia.number--;
       
-      if (trivia.q>=0){
+      
             if (trivia.number>0){
               $("#time-remaining").html("<h2>" + trivia.number + "</h2>");
             }
@@ -89,20 +89,23 @@ var datos ={
               $("#time-remaining").html("<h2>" + trivia.number + "</h2>");
               trivia.number=30;
               trivia.q--;
-              datos.fillquestion(trivia.q);
-              
+              if (trivia.q>=0){
+                datos.fillquestion(trivia.q);
+              }
+              else{
+                trivia.q=9;
+               clearInterval(trivia.intervalId);
+                $("#answer1").html("<h3> Correct answers : "+trivia.good+"</h3>");
+                $("#answer2").html("<h3> Incorrect answers : "+trivia.bad+"</h3>");   
+                $("#answer3").html("<h3> Unanswered : "+trivia.what+"</h3>");
+                $("#answer4").html('<button type="button" id="start" class="btn btn-outline-secondary">  START  </button>');
+       //         alert("juego terminado");
+                trivia.stage=0;
+              }   
+
             }
-     }
-      else{
-            trivia.q=9;
-           clearInterval(trivia.intervalId);
-            $("#answer1").html("<h3> Correct answers : "+trivia.good+"</h3>");
-            $("#answer2").html("<h3> Incorrect answers : "+trivia.bad+"</h3>");   
-            $("#answer3").html("<h3> Unanswered : "+trivia.what+"</h3>");
-            $("#answer4").html('<button type="button" id="start" class="btn btn-outline-secondary">  START  </button>');
-            alert("juego terminado");
-            trivia.stage=0;
-          }
+     
+
       
     },
     intervalId:null
@@ -124,8 +127,11 @@ $("#answer3").on("click",function(){
 });
 $("#answer4").on("click",function(){
   if (trivia.stage==0){
-    debugger;
+
     trivia.q=9;
+    trivia.good=0;
+    trivia.bad=0;
+    trivia.what=10;
     datos.fillquestion(trivia.q);
     trivia.run();
 
